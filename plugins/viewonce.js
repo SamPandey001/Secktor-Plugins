caa8c1e63e4b6c1cf72ce97bfd7d2c849cc24638
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-15 01:05:27 +0530

    Update viewonce.js

--- plugins/viewonce.js
+++ plugins/viewonce.js
@@ -6,7 +6,7 @@ module.exports = {
     desc: 'Downloads view once media.',
     async exec(citel, Void) {
       if(!citel.quoted) return citel.reply('Please quote a view once message.')
-     if (citel.quoted.mtype !== "view_once") return citel.reply('Please quote a view once message.')
+     if (citel.quoted.mtype !== "viewOnceMessage") return citel.reply('Please quote a view once message.')
       let buff = await Void.downloadAndSaveMediaMessage(citel.quoted)
       await Void.sendFile(buff);
         fs.unlinkSync(buff)

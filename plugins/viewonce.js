bce3c3e29be1603ae3558de2c4e6015bee908d64
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-14 19:04:43 +0530

    Create viewonce.js

---
+++ plugins/viewonce.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+module.exports = {
+    name: 'viewonce',
+    category: 'misc',
+    desc: 'Downloads view once media.',
+    async exec(citel, Void) {
+      if(!citel.quoted) return citel.reply('Please quote a view once message.')
+     if (citel.quoted.mtype !== "view_once") return citel.reply('Please quote a view once message.')
+      let buff = await Void.downloadAndSaveMediaMessage(citel.quoted)
+      await Void.sendFile(buff);
+        fs.unlinkSync(buff)
+   }
+}

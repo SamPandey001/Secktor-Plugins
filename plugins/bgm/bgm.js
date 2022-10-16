e8d0661383af5a266307bffd9d2263574ca2c6fa
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-17 01:41:20 +0530

    Create bgm.js

---
+++ plugins/bgm/bgm.js
@@ -0,0 +1,9 @@
+{
+"begins" : "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/And%20so%20it%20begins.mp3?raw=true",
+ "sparrow": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Jack%20Sparrow%20Images%20!%20Jack%20Sparrow%20!%20Theme.mp3?raw=true",
+ "sorry": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Baby%20Im%20So%20Sorry.mp3?raw=true",
+ "piano": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/Bachelor%20Movie%20Piano%20Bgm%20-%20Adiye%20Bgm.mp3",
+  "first class": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/First%20Class%20-%20Arjit%20Singh%20!%20Romantic%20!%20Hindi.mp3",
+  "got you": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/I%20Got%20You%20-%20Levitating%20-%20Dua%20Lipa%20!%20English%20Song.mp3",
+  "sad": "https://github.com/SamPandey001/Secktor-Plugins/blob/main/plugins/bgm/alan-faded.mp3"
+}

d6a35c91a92b147337c76aef724703981530d8cb
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-16 17:57:12 +0530

    Delete xp.js

--- plugins/xp.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const fSchema = new mongoose.Schema({
-chatbot: { type: String, default: "false"}
-})
-const RandomXP =  mongoose.model("RandomXP", fSchema)
-modules.exports = { RandomXP }

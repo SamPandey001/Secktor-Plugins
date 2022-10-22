528f95b82e2a474b7bf14d6d3860795de4908fcc
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-22 09:55:40 +0530

    Update doggy.js

--- plugins/doggy.js
+++ plugins/doggy.js
@@ -1,10 +1,3 @@
-/*
-------------------------------------
-Copyright (C) 2022 Secktor-Md.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-------------------------------------
-*/
 module.exports = {
     name: 'doggy',
     category: 'troll',

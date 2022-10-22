acb4af7b4409f9bff748b60096b70ec1a1df0ed4
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-10-22 09:57:08 +0530

    Update doggy.js

--- plugins/doggy.js
+++ plugins/doggy.js
@@ -3,7 +3,7 @@ module.exports = {
     category: 'troll',
     desc: 'Makes troll image of given text.',
     async exec(citel, Void,args) {
-     const url = `https://docs-jojo.herokuapp.com/api/meme-gen?top=${args.join(' ').split(";")[0]}&bottom=${args.join(' ').split(";")[1]&img=https://static.wikia.nocookie.net/dogelore/images/9/97/Doge.jpg/revision/latest/top-crop/width/360/height/450?cb=20190205113053`
+     const url = `https://docs-jojo.herokuapp.com/api/meme-gen?top=${args.join(' ').split(";")[0]}&bottom=${args.join(' ').split(";")[1]}&img=https://static.wikia.nocookie.net/dogelore/images/9/97/Doge.jpg/revision/latest/top-crop/width/360/height/450?cb=20190205113053`
       citel.imgurl(url,'Secktor Troll Pack(doggy)')
    }
 }
